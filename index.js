0e7ddcea37ec5b908bd2827e2fd78463c6f3fc82
The Shining Dazzle <zommerberg@abv.bg>
2019-01-05 23:18:57 +0200

    new

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const app = express();
+const keys = require("./config/keys");
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "./auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
 
 app.get("/", (req, res) => {
   res.send({ xd: "lmao" });
